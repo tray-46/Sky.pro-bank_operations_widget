ea9d95f728293a4831b51d43726d404e5f2b5652
tray <at-box@mail.ru>
2026-02-02 19:38:37 +0500

    initial project commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# back end of widget
+get limited list of user's last successful bank operations
---
+++ src/masks.py
@@ -0,0 +1,9 @@
+def get_mask_card_number(card_number: int) -> str:
+    """function to get masked bank card number"""
+    str_card_number = str(card_number)
+    return str_card_number[0:4] + " " + str_card_number[4:6] + "** **** " + str_card_number[-4:]
+
+
+def get_mask_account(account_number: int) -> str:
+    """function to get masked bank account number"""
+    return "**" + str(account_number)[-4:]
