f5e9413d94e62f3d35e3c1d640dc1feb5d12ec05
tray <at-box@mail.ru>
2026-02-09 12:33:40 +0500

    linted with: flake8, mypy, isort and black

--- src/processing.py
+++ src/processing.py
@@ -8,4 +8,6 @@ def filter_by_state(operations_list: list[dict], targeted_state: str = "EXECUTED
 
 def sort_by_date(operations_list: list[dict], *, descending_sorting: bool = True) -> list[dict]:
     """function for sorting operations list by date"""
-    return sorted(operations_list, key=lambda operation: operation.get('date'), reverse = descending_sorting)
+    return sorted(
+        operations_list, key=lambda operation: operation.get("date", "1970-01-01T00:00:00"), reverse=descending_sorting
+    )
