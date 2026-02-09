49cfcfc4dce6f0cfd2274e65e79346ae8fdfa1b4
tray <at-box@mail.ru>
2026-02-09 12:05:36 +0500

    sort_by_date test added

--- src/processing.py
+++ src/processing.py
@@ -6,6 +6,6 @@ def filter_by_state(operations_list: list[dict], targeted_state: str = "EXECUTED
     return [operation for operation in operations_list if operation.get("state", "NO_STATE_KEY") == targeted_state]
 
 
-def sort_by_date(operations_list: list[dict], reverse_sorting: bool = False) -> list[dict]:
+def sort_by_date(operations_list: list[dict], *, descending_sorting: bool = True) -> list[dict]:
     """function for sorting operations list by date"""
-    return sorted(operations_list, key=lambda operation: operation.get('date'), reverse=reverse_sorting)
+    return sorted(operations_list, key=lambda operation: operation.get('date'), reverse = descending_sorting)
--- tests/widget_test.py
+++ tests/widget_test.py
@@ -47,6 +47,7 @@ print(f"\nAccount|Card number:\t{number_to_mask}")
 print(f"Masked number:\t\t\t{widget.mask_account_card(number_to_mask)}")
 
 # get_date test
+print("\nget_date test:")
 datetime_string = "2024-03-11T02:26:16.671407"
 print(f"\ndatetime string is:\t{datetime_string}")
 print(f"Formated date is:\t{widget.get_date(datetime_string)}")
