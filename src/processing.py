e7be03a18bbbcddd47b19d13a3d49912b1530fcd
tray <at-box@mail.ru>
2026-02-09 11:27:15 +0500

    functions filter_by_state and sort_by_date added

---
+++ src/processing.py
@@ -0,0 +1,11 @@
+"""modul with functions for data processing"""
+
+
+def filter_by_state(operations_list: list[dict], targeted_state: str = "EXECUTED") -> list[dict]:
+    """function for filtering operations list by targeted state"""
+    return [operation for operation in operations_list if operation.get("state", "NO_STATE_KEY") == targeted_state]
+
+
+def sort_by_date(operations_list: list[dict], reverse_sorting: bool = False) -> list[dict]:
+    """function for sorting operations list by date"""
+    return sorted(operations_list, key=lambda operation: operation.get('date'), reverse=reverse_sorting)
