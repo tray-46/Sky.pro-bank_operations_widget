54a8a485a879bf3cd85b82c432e09f9661fab0e9
tray <at-box@mail.ru>
2026-02-12 11:38:53 +0500

    test_widget added

---
+++ tests/test_widget.py
@@ -0,0 +1,27 @@
+"""test_widget.py with tests for widget.py module functions"""
+import pytest
+
+from src import widget
+
+@pytest.mark.parametrize(
+    "number_to_mask, expected",
+    [
+        ("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"),
+        ("Maestro 7000792289606361", "Maestro 7000 79** **** 6361"),
+        ("Счет 73654108430135874305", "Счет **4305"),
+        ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+        ("Счет 64686473678894779589", "Счет **9589"),
+        ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
+        ("Счет 35383033474447895560", "Счет **5560"),
+        ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
+        ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
+        ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
+        ("Счет 73654108430135874305", "Счет **4305")
+     ]
+)
+def test_mask_account_card(number_to_mask, expected):
+    assert widget.mask_account_card(number_to_mask) == expected
+
+
+def test_get_date():
+    assert widget.get_date("2024-03-11T02:26:16.671407") == "11.03.2024"
