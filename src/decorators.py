42afb9c22820c78ec70a766889d92324aafd4935
tray <at-box@mail.ru>
2026-02-21 17:19:21 +0500

    lint with flake8, isort and mypy

--- src/decorators.py
+++ src/decorators.py
@@ -2,13 +2,17 @@
 
 from functools import wraps
 from pathlib import Path
-import typing
+from typing import Callable, ParamSpec, TypeVar
 
-def log(filename: str | None = None) -> typing.Callable:
+P = ParamSpec("P")
+T = TypeVar("T")
+
+
+def log(filename: str | None = None) -> Callable:
     """Decorator for logging function execution"""
-    def decorator(function):
+    def decorator(function: Callable[P, T]) -> Callable[P, T]:
         @wraps(function)
-        def wrapper(*args, **kwargs):
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
             if filename is not None:
                 file_path = Path(f"{Path(__file__).parent.parent}/logs/{filename}")
                 file_path.parent.mkdir(parents=True, exist_ok=True)
