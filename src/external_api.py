d071bb7f0a700602f88868b65464ceb7a1624f4a
tray <at-box@mail.ru>
2026-03-03 10:52:04 +0500

    convert_to_rub added

---
+++ src/external_api.py
@@ -0,0 +1,31 @@
+"""module with functions working with external APIs"""
+
+import os
+import requests
+
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("EXCHANGE_RATES_API_KEY")
+
+
+def convert_to_rub(from_currency: str, amount: float) -> float:
+    """
+    function to convert amount in given currency to rubles
+    :param from_currency: three-letter currency code of the currency to convert from
+    :param amount: amount to be converted
+    :return: amount converted to rubles
+    """
+    url = "https://api.apilayer.com/exchangerates_data/convert"
+    payload = {
+        "amount": amount,
+        "from": from_currency,
+        "to": "RUB"
+    }
+    headers = {
+        "apikey": API_KEY
+    }
+    response = requests.get(url, params=payload, headers=headers)
+    if response.status_code == 200:
+        return round(response.json()["result"], 2)
+    raise Exception(f"Conversion failed: {from_currency} {amount}")
