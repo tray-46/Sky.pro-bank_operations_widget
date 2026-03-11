6d1f9bfcaf4a427b1c3d7fd1399fd2bc95f9c62b
tray <at-box@mail.ru>
2026-03-11 11:07:33 +0500

    logging for masks.py, utils.py and external_api.py modules added

--- src/external_api.py
+++ src/external_api.py
@@ -2,12 +2,21 @@
 
 import os
 
+import logging
+import pathlib
 import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 API_KEY = os.getenv("EXCHANGE_RATES_API_KEY")
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+log_file_path = str(pathlib.Path(__file__).parent.parent / "logs" / "external_api.log")
+file_handler = logging.FileHandler(log_file_path, mode="w", encoding="utf-8")
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 def convert_to_rub(from_currency: str, amount: float) -> float:
     """
@@ -16,11 +25,25 @@ def convert_to_rub(from_currency: str, amount: float) -> float:
     :param amount: amount to be converted
     :return: amount converted to rubles
     """
+    logger.debug(f"Function convert_to_rub called with from_currency: {from_currency} and amount: {amount}")
     url = "https://api.apilayer.com/exchangerates_data/convert"
     payload = {"amount": amount, "from": from_currency, "to": "RUB"}
     headers = {"apikey": API_KEY}
-    response = requests.get(url, params=str(payload), headers=headers)
-    if response.status_code == 200:
-        result: float = round(response.json()["result"], 2)
-        return result
+    try:
+        logger.info(f"Executing request to Exchange Rates Data API")
+        response = requests.get(url, params=str(payload), headers=headers)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Function get_transaction_amount failed due to exception: {e}")
+        raise e
+    try:
+        logger.info(f"Processing the response from Exchange Rates API")
+        if response.status_code == 200:
+            result: float = round(response.json()["result"], 2)
+            logger.info(f"Function convert_to_rub completed successfully with result: {result}")
+            return result
+    except Exception as e:
+        logger.error(f"Function get_transaction_amount failed due to exception: {e}")
+        raise e
+    logger.error(f"Function get_transaction_amount failed")
     raise Exception(f"Conversion failed: {from_currency} {amount}")
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,19 @@
 """modul with utility functions"""
 
 import json
+import logging
 import pathlib
 
 import src.external_api
 
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+log_file_path = str(pathlib.Path(__file__).parent.parent / "logs" / "utils.log")
+file_handler = logging.FileHandler(log_file_path, mode="w", encoding="utf-8")
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 def load_transactions_data(file_path_str: str) -> list[dict]:
     """
@@ -12,14 +21,24 @@ def load_transactions_data(file_path_str: str) -> list[dict]:
     :param file_path_str: string with path to JSON file
     :return: transactions data loaded from JSON file, if file not found, empty or contain not a list return empty list
     """
+    logger.debug(f"Function load_transactions_data called with file_path_str: {file_path_str}")
     file_path = pathlib.Path(file_path_str)
     if not file_path.is_file() or file_path.stat().st_size == 0:
+        logger.info(f"Specified file was not found or is empty. Returning empty list")
         return []
-    with open(file_path, "r", encoding="utf-8") as json_file:
-        transactions_data = json.load(json_file)
-        if not isinstance(transactions_data, list):
-            return []
-        return transactions_data
+    try:
+        logger.debug(f"Reading file: {file_path_str}")
+        with open(file_path, "r", encoding="utf-8") as json_file:
+            logger.debug(f"Loading json data")
+            transactions_data = json.load(json_file)
+            if not isinstance(transactions_data, list):
+                logger.info(f"Data read from {file_path_str} is not a list object. Returning empty list")
+                return []
+            logger.info(f"Data read from {file_path_str} is successful. Returning transaction list")
+            return transactions_data
+    except Exception as e:
+        logger.error(f"Function load_transactions_data failed due to exception: {e}. Returning empty list")
+        return []
 
 
 def get_transaction_amount(transaction: dict) -> float:
@@ -29,8 +48,17 @@ def get_transaction_amount(transaction: dict) -> float:
     :param transaction: dict with transaction data
     :return: transaction amount in rubles
     """
-    currency_code = transaction["operationAmount"]["currency"]["code"]
-    amount = float(transaction["operationAmount"]["amount"])
-    if currency_code != "RUB":
-        amount = src.external_api.convert_to_rub(currency_code, amount)
-    return amount
+    try:
+        logger.debug(f"Function get_transaction_amount called")
+        logger.debug(f"Getting currency code from transaction data")
+        currency_code = transaction["operationAmount"]["currency"]["code"]
+        logger.debug(f"Getting amount from transaction data")
+        amount = float(transaction["operationAmount"]["amount"])
+        if currency_code != "RUB":
+            logger.info(f"Converting amount from {currency_code} to rubles with convert_to_rub function")
+            amount = src.external_api.convert_to_rub(currency_code, amount)
+        logger.info(f"Getting transaction amount is successful. Returning amount: {amount}")
+        return amount
+    except Exception as e:
+        logger.error(f"Function get_transaction_amount failed due to exception: {e}")
+        raise e
