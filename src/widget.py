2a617c6fcb05fbc06a76452fe5905921699cca1f
tray-46 <at-box@mail.ru>
2026-03-19 16:19:55 +0500

    Revert "Homework 13.1 done"

--- src/widget.py
+++ src/widget.py
@@ -1,38 +1,21 @@
 """modul with basic functions of widget"""
 
-import datetime
-
-from src import masks
-
 
 def mask_account_card(string_to_mask: str) -> str:
     """function to get masked bank account or card number"""
-    if not isinstance(string_to_mask, str):
-        raise TypeError("Invalid account/card number string")
+    import src.masks
+
     string_parts = string_to_mask.split()
-    if len(string_parts) < 2:
-        raise ValueError("Invalid account/card number")
     if string_parts[0] == "Счет":
-        try:
-            account_number = int(string_parts[-1])
-            return f"{string_parts[0]} {masks.get_mask_account(account_number)}"
-        except ValueError:
-            raise ValueError("Invalid account number")
+        return f"{string_parts[0]} {src.masks.get_mask_account(int(string_parts[-1]))}"
     else:
-        try:
-            card_number = int(string_parts[-1])
-            return f"{" ".join(string_parts[:-1])} {masks.get_mask_card_number(card_number)}"
-        except ValueError:
-            raise ValueError("Invalid card number")
+        return f"{" ".join(string_parts[:-1])} {src.masks.get_mask_card_number(int(string_parts[-1]))}"
 
 
 def get_date(date_string: str) -> str:
     """function to get formatted date string. required format is 'dd.mm.yyyy'"""
-    if not isinstance(date_string, str):
-        raise TypeError("Invalid date string")
+    import datetime
+
     datetime_format = "%Y-%m-%dT%H:%M:%S.%f"
-    try:
-        datetime_date = datetime.datetime.strptime(date_string, datetime_format)
-    except ValueError:
-        raise ValueError("Invalid date format")
+    datetime_date = datetime.datetime.strptime(date_string, datetime_format)
     return datetime_date.strftime("%d.%m.%Y")
